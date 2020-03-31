0ca751fcfe44831f136c0e6df9d89393037cea46
jonathansh1115 <30495257+jonathansh1115@users.noreply.github.com>
2020-03-31 20:51:59 +0800

    .

--- api/includes/headerssss.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-header('Access-Control-Allow-Origin: https://itsmejonanathan.netlify.com');
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");
-header("Content-type:application/json");
-
-?>
